3e16fda85baa77fdb485eeab4179609d6515dc3f
HeshanHH <heshanwijerathna01@gmail.com>
2021-05-08 16:18:53 +0530

    Add CartScreen Markups and CartItem Component

---
+++ frontend/src/components/CartItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './CartItem.css';
+
+const CartItem = () => {
+  return <div className="cartitem"></div>;
+};
+
+export default CartItem;
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './CartScreen.css';
 
+// Components
+import CartItem from '../components/CartItem';
+
 const CartScreen = () => {
   return (
-    <div className="cartscreen">
-      <h1>Cart Screen</h1>
+    <div>
+      <div className="cartscreen">
+        {/* left */}
+        <div className="cartscreen__left">
+          <h2>Shopping Cart</h2>
+          <CartItem></CartItem>
+        </div>
+        {/* rigth */}
+        <div className="cartscreen__right">
+          <div className="cartscreen__info">
+            <p>Subtotal (10) items</p>
+            <p>$1100.10</p>
+          </div>
+          <div>
+            <button>Proceed To Checkout</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
-
+// screen divide in to two parts
 export default CartScreen;

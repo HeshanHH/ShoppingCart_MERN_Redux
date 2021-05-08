8a4b35fbb5f2bdd84428facab98c29a4f824a9a6
HeshanHH <heshanwijerathna01@gmail.com>
2021-05-08 14:37:20 +0530

    Add Sidedrawe styling and backdrop dynamic toggles

--- frontend/src/components/Backdrop.js
+++ frontend/src/components/Backdrop.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Backdrop.css';
 // to desable the other part of the webpage.
-export const Backdrop = ({ show }) => {
-  return show && <div className="backdrop"></div>;
+export const Backdrop = ({ show, click }) => {
+  return show && <div className="backdrop" onClick={click}></div>;
 };
 
 export default Backdrop;

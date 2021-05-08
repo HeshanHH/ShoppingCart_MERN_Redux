47d33eabdb84847865fe7de66585e7790e9fbfa9
HeshanHH <heshanwijerathna01@gmail.com>
2021-05-08 14:57:57 +0530

    Add Product component structure

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Product from '../components/Product';
 import './HomeScreen.css';
 
 const HomeScreen = () => {
   return (
     <div className="homescreen">
       <h2 className="homescreen__title">Latest Products</h2>
-      <div className="homescreen__products">{/* Display prodcts */}</div>
+      <div className="homescreen__products">
+        {/* Display prodcts */}
+        <Product></Product>
+      </div>
     </div>
   );
 };
